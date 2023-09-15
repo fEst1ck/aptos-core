93ce0152353c6fa58f389205768c19c0be209e3c
Larry Liu <larry@aptoslabs.com>
2023-09-15 11:28:28 -0700

    data service v2

--- ecosystem/indexer-grpc/indexer-grpc-data-service/src/lib.rs
+++ ecosystem/indexer-grpc/indexer-grpc-data-service/src/lib.rs
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod config;
+pub mod grpc_response_stream;
 pub mod metrics;
+pub mod response_dispatcher;
 pub mod service;
 
 pub use config::{IndexerGrpcDataServiceConfig, NonTlsConfig, SERVER_NAME};
